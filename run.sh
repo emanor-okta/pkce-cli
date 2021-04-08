b7249d1021d408cdfa6682faa470ce1da59588fb
Erik Manor <erik.manor@okta.com>
2021-04-07 17:47:06 -0700

    modify to use authn, no browser

---
+++ run.sh
@@ -0,0 +1,7 @@
+./pkce-cli \
+  --client_id 0oa1ddcipQGRBMQ815d6 \
+  --okta_org https://erikmanor.okta.com \
+  --scopes "openid profile email" \
+  --redirect_uri http://localhost:8080/redirect \
+  --user "test.user@mail.com" \
+  --pass "s1mpleP@\$\$" 
